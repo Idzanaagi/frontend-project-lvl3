dea04fd0b5c106053efe2f1ba3704f0efe8bb02a
Idzanaagi <anmorgunowa@gmail.com>
2022-06-25 02:06:18 +0300

    add i18next

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
-// import i18next from 'i18next';
-// import { setLocale } from 'yup';
-// import resources from './locales/index.js';
+import i18next from 'i18next';
+import { setLocale } from 'yup';
+import ru from './locales/index.js';
 import validateForm from './validator.js';
 // import render from './view.js';
 import view from './view.js';
@@ -16,7 +16,25 @@ const app = () => {
     feedList: [],
   };
 
-  const watchedState = view(state);
+  const i18n = i18next.createInstance();
+  i18n.init({
+    lng: 'ru',
+    debug: false,
+    resources: {
+      ru,
+    },
+  }).then(() => {
+    setLocale({
+      mixed: {
+        notOneOf: () => i18n.t('errors.alreadyExist'),
+      },
+      string: {
+        url: () => i18n.t('errors.notValidInput'),
+      },
+    });
+  });
+
+  const watchedState = view(state, i18n);
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
