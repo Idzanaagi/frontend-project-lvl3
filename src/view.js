533d76cc33b5d42c7621c8610a0a6989395b1977
Idzanaagi <anmorgunowa@gmail.com>
2022-06-25 02:21:25 +0300

    step3: add basic output i18next

--- src/view.js
+++ src/view.js
@@ -1,17 +1,20 @@
 import onChange from 'on-change';
 
 const input = document.querySelector('#url-input');
+const feedback = document.querySelector('.feedback');
 
-const view = (obj) => {
+const view = (obj, i18n) => {
   const watchedObj = onChange(obj, (path, value) => {
     if (path === 'RssForm.state') {
       if (value === 'failed') {
         input.style.border = 'thick solid red';
+        feedback.textContent = i18n.t('errors.notValidInput');
       }
       if (value === 'finished') {
         input.style.border = null;
         input.value = '';
         input.focus();
+        feedback.textContent = i18n.t('successful.download');
       }
     }
   });
